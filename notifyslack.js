f0492d7583e765b23e31d9cca4d9c7e87821dc07
Scott Evans <git@scott.ee>
2014-12-17 10:28:55 +0000

    Initial commit

---
+++ notifyslack.js
@@ -0,0 +1,196 @@
+// *********************** //
+// NotifySlack v0.00000001 //
+// *********************** //
+
+// to-do: basic front end template for getting setup
+// 		  fix issue with marking notifications as seen
+//        implement a storage mechanism/make it into an app? requires authentication etc (too far?)
+
+// app settings
+
+var settings = {
+	'wp_client_id': '',
+	'wp_client_secret': '',
+	'slack_hook': '',
+	'slack_domain': '',
+	'slack_channel': '#channel',
+	'oauth_token': ''
+};
+
+// express
+
+var express = require('express'),
+app = express();
+
+// slack
+
+var Slack = require('node-slack');
+var slack = new Slack(settings.slack_hook);
+
+// cron
+
+var CronJob = require('cron').CronJob;
+
+// oauth settings
+
+var oauth2 = require('simple-oauth2')({
+	clientID: settings.wp_client_id,
+	clientSecret: settings.wp_client_secret,
+	grant_type: 'authorization_code',
+	site: 'https://public-api.wordpress.com',
+	authorizationPath: '/oauth2/authorize',
+	tokenPath: '/oauth2/token'
+});
+
+// auth uri definition
+
+var authorization_uri = oauth2.authCode.authorizeURL({
+	redirect_uri: 'http://localhost:3000/callback'
+});
+
+// boot the app
+
+var server = app.listen(3000, function () {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('NotifySlack (http://%s:%s)', host, port)
+});
+
+// default view
+
+app.get('/', function (req, res) {
+	console.log('/');
+	if (settings.oauth_token == '') {
+		res.send('NotifySlack App - <a href="/auth">Grab your oAuth token</a>.');
+	} else {
+		res.send('NotifySlack App');
+	}
+});
+
+// redirect to /auth with WordPress.com to get token
+
+app.get('/auth', function (req, res) {
+
+	// once an auth key is set bail
+	if (settings.oauth_token != '')
+		res.redirect('/'); return;
+
+	console.log(authorization_uri);
+	res.redirect(authorization_uri);
+});
+
+// callback service parsing the authorisation token and asking for the access token
+
+app.get('/callback', function (req, res) {
+
+	// once an auth key is set bail
+	if (settings.oauth_token != '')
+		res.redirect('/'); return;
+
+	var code = req.query.code;
+	console.log('/callback');
+	oauth2.authCode.getToken({
+		code: code,
+		redirect_uri: 'http://localhost:3000/callback'
+	}, saveToken);
+
+	function saveToken(error, result) {
+		if (error) { console.log('Access Token Error', error.message); }
+			token = oauth2.accessToken.create(result);
+			console.log(token);
+			res.redirect('/')
+	}
+});
+
+// run the sweep every minute when a token is set
+
+new CronJob('0 * * * * *', function() {
+
+	if (settings.oauth_token == '' )
+		return;
+
+	console.log('Running NotifySlack...');
+
+	var rest = require('restler');
+
+	var options = {
+		accessToken: settings.oauth_token
+	};
+
+	rest.get('https://public-api.wordpress.com/rest/v1/notifications/?unread=true', options).on('complete', function(result, response) {
+
+		if (result instanceof Error) {
+
+			console.log('Error:', result.message);
+
+		} else {
+
+			var counts = {};
+			var ent = require('ent');
+
+		 	result.notes.forEach(function(note) {
+
+				console.log('Sending ' + note.id + ' to Slack.');
+
+				switch(note.type) {
+
+					case "like_milestone_achievement":
+
+						var text = ent.decode(note.subject.text);
+						var title = 'WordPress.com: ' + ent.decode(note.body.header_text);
+
+						break;
+
+					default:
+
+						var text =  '<'+ note.body.header_link +'|'+ ent.decode(note.subject.text) +' »>';
+						var title = 'WordPress.com: ' + ent.decode(note.body.header_text);
+				}
+
+				// send to slack
+				slack.send({
+					text: text,
+					channel: settings.slack_channel,
+					username: title,
+					icon_url: note.subject.icon,
+					unfurl_links: true,
+					link_names: 1
+				});
+
+				// add to array mark as read
+				counts[note.id] = 1;
+
+			});
+
+			// mark these as read
+			// console.log(counts);
+
+			if (Object.keys(counts).length) {
+
+				var postoptions = {
+					data: {
+						counts: counts
+					},
+					accessToken: settings.oauth_token
+				};
+
+				rest.post('https://public-api.wordpress.com/rest/v1/notifications/read', postoptions).on('complete', function(postresult, postresponse) {
+
+					if (postresult instanceof Error) {
+
+						console.log('Error:', postresult.message);
+
+					} else {
+
+						console.log(Object.keys(counts).length + ' notifications marked as read.');
+						console.log(postresult);
+					}
+				});
+			} else {
+				console.log('No new notifications.')
+			}
+		}
+	});
+}, null, true);
---
+++ readme.md
@@ -0,0 +1,67 @@
+# NotifySlack
+
+This small node.js app will send your personal WordPress.com notifications to a Slack room.
+
+To get setup you will need:
+
+- To register a WordPress.com App
+- Create a custom Slack Hook
+
+## Setup the App
+
+Browse to the directory in your terminal app and run: `npm install`.
+
+## Register a WordPress.com App
+
+Visit the [WordPress.com Developer site](https://developer.wordpress.com/apps/) and register a new app. Set the redirect URI to `http://localhost:3000` and leave the JavaScript origins section blank. The rest of the information can be personalised to your own requirements.
+
+Once setup make a note of the `Client ID` and `Client Secret` and add those to the settings array at the top of `notifyslack.js`.
+
+You can now obtain your oAuth token. Launch the app:
+
+`node notifyslack.js`
+
+and browse to:
+
+`localhost:3000`
+
+Follow the instructions on screen and keep an eye on your terminal. Once you have approved the app you will see your oAuth token in your terminal:
+
+```
+{ create: [Function: create],
+	token:
+	{ access_token: 'YOUR SECRET',
+	token_type: 'bearer',
+	blog_id: '0000000000',
+	blog_url: 'http://site.wordpress.com',
+	scope: '',
+	expires_at: Wed Dec 17 2014 09:54:36 GMT+0000 (GMT) },
+	expired: [Function: expired],
+	refresh: [Function: refresh],
+	revoke: [Function: revoke]
+}
+```
+
+Copy your access token and add it to the settings array at the top of `notifyslack.js`.
+
+## Create a custom Slack hook
+
+Visit the [Slack website](https://slack.com/services/new/incoming-webhook) and create your custom webhook. Then add the URL to the settings array at the top of `notifyslack.js`. You can also add your `slack_channel` (the room you wish to post to) and `slack_domain` (your company wide domain for your Slack account e.g: `wordpress`).
+
+## Set it Running
+
+Quit the node app with `CTRL-C` and then set it running again: `node notifyslack.js`.
+
+You should now see the following in your terminal.
+
+```
+NotifySlack (http://0.0.0.0:3000)
+Running NotifySlack...
+No new notifications.
+```
+
+The app will check for new notifications every minute and post them to your Slack room.
+
+## Bugs
+
+At the moment the app does not correctly set the messages as seen. This is being worked on.
